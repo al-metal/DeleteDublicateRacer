de007a8d28d1764e7c78609fcafc63b99bf55c16
al-metal <al-metal@users.noreply.github.com>
2017-07-10 15:08:33 +0400

    Рабочий алгоритм

--- Form1.cs
+++ Form1.cs
@@ -50,10 +50,46 @@ namespace DeleteDublicateRacer
             string otv = webRequest.getRequest("https://bike18.ru/products/category/katalog-zapchastey-racer");
             MatchCollection razdel = new Regex("(?<=</div></a><div class=\"category-capt-txt -text-center\"><a href=\").*?(?=\" class=\"blue\">)").Matches(otv);
 
-            for(int i = 0; razdel.Count > i; i++)
+            for (int i = 0; razdel.Count > i; i++)
             {
+                List<string> uncalTovar = new List<string>();
+                List<string> deleteTovar = new List<string>();
+                string urlRazdel = "https://bike18.ru" + razdel[i].ToString() + "?page=all";
+                otv = webRequest.getRequest(urlRazdel);
+                MatchCollection tovarURL = new Regex("(?<=<div class=\"product-link -text-center\"><a href=\").*(?=\" >)").Matches(otv);
+                for (int n = 0; tovarURL.Count > n; n++)
+                {
+                    string tovarUrlStr = tovarURL[n].ToString();
+                    string nameTovar = new Regex("(?<=<a href=\"" + tovarUrlStr + "\" >).*?(?=</a>)").Match(otv).ToString();
+                    bool b = false;
+                    foreach (string s in uncalTovar)
+                    {
+                        if (s == nameTovar)
+                        {
+                            b = true;
+                        }
+                    }
 
+                    if (b)
+                    {
+                        deleteTovar.Add(tovarUrlStr);
+                    }
+                    else
+                    {
+                        uncalTovar.Add(nameTovar);
+                    }
+                }
+                if (deleteTovar.Count != 0)
+                {
+                    foreach(string url in deleteTovar)
+                    {
+                        List<string> product = nethouse.GetProductList(cookie, url);
+                        nethouse.DeleteProduct(cookie, url);
+                    }
+
+                }
             }
+            MessageBox.Show("Все готово");
         }
 
         private void Form1_Load(object sender, EventArgs e)
